366c17779466de8518bd2e7a80435999e5734b9c
AleCar007 <aonotsukunenihon@gmail.com>
2024-01-18 15:07:08 -0600

    Agruegando componente Hola mundo

--- mi-primer-proyecto-vite/src/components/HolaMundo.js
+++ mi-primer-proyecto-vite/src/components/HolaMundo.js
@@ -1,6 +1,9 @@
 const HolaMundo = () => `
     <h1>Hola Mundo</h1>
-    <p>Este es un componente de prueba</p>
+    <p>Este es un componente de prueba ${algo}</p>
 `
+
+const algo = `Algo`
+
 
 export default HolaMundo
