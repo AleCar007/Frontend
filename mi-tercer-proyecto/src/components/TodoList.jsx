6b01568d46950481b458f65c2c7bab50fc61aa9b
AleCar007 <aonotsukunenihon@gmail.com>
2024-01-25 13:29:39 -0600

    main

--- mi-tercer-proyecto/src/components/TodoList.jsx
+++ mi-tercer-proyecto/src/components/TodoList.jsx
@@ -1,8 +1,32 @@
+import { useState } from "react"
+
+
 const TodoList = () => {
+    const [inputValue, setInputValue] = useState('')
+
+    const handleAddItem = () => {
+      console.log("nueva tarea; ", inputValue)
+    }
+
   return (
     <div>
         <h1>Lista de Tareas</h1>
-
+        {/*Para tomar control de la informacion que hay en el input, podemos hacer uso del evento onChange
+        para guarda la informacion del input y a penas sea teclado y guardarla en mi estado*/} 
+        <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+        <hr />
+        <button onClick={handleAddItem}>Agregar</button>
+        <ul>
+          <li>Comprar despensa
+            <button>Eliminar</button>
+          </li>
+          <li>Lavar ropa
+            <button>Eliminar</button>
+          </li>
+          <li>Pasear perro
+            <button>Eliminar</button>
+          </li>
+        </ul>
         
     </div>
   )
