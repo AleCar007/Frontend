8e5c163f4364a1d18e27ffe37117b3cc6aa10030
AleCar007 <aonotsukunenihon@gmail.com>
2024-02-23 14:30:14 -0600

    redirects

--- mi-quinto-proyecto/src/components/NavBar.jsx
+++ mi-quinto-proyecto/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className='navbar navbar-expand-sm navbar-light bg-warning d-flex justify-content-between px-4'>
-      <NavLink className='navbar-brand' to='#'>Pokédex</NavLink>
+      <NavLink className='navbar-brand' to='/'>Pokédex</NavLink>
 
       <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
         <li className='nav-item px-2'>
